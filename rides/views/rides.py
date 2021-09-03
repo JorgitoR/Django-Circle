c638b0153ea54eb13e7609ea5bfc5164378c25af
unknown <jorgitouribe@gmail.com>
2021-09-03 10:28:53 -0500

    Django circle

--- rides/views/rides.py
+++ rides/views/rides.py
@@ -5,6 +5,7 @@ from rest_framework import mixins, viewsets
 
 # Permissions
 from rest_framework.permissions import IsAuthenticated
+from cride.circles.permissions.memberships import IsActiveCircleMember
 
 # Serializers
 from cride.rides.serializer import CreateRideSerializer
@@ -16,3 +17,6 @@ from cride.circles.models import Circle
 class RideViewSet(mixins.CreateModelMixin,
 				  viewsets.GenericViewSet):
 	"""Ride view set."""
+
+	serializer_class = CreateRideSerializer
+	permission_clasess = [IsAuthenticated, IsActiveCircleMember]
